9696bc4e6bb05883be243ecd94bf1e6c0d496cf4
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-05-11 18:55:06 -0400

    modify imports

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ The primary application script for Bike Share Sim.
 
 This is the module with the application code.
 """
-from sim import Simulation
+from sim.sim import Simulation
 
 # Application code
 if __name__ == '__main__':
