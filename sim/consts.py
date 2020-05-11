38dd6e2ce7c772a438b984029ad3deca731dbbca
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-05-11 17:35:34 -0400

    add lambda and length

--- sim/consts.py
+++ sim/consts.py
@@ -1,11 +1,18 @@
-### DOCK AND BIKE ###
+### DOCK AND BIKE CONSTANTS ###
+
 CONDITIONS = ['good', 'fair', 'poor', 'not in service']
 
-
 ### BIKE CONSTANTS ###
+
 CLASSIC_BASE_RATE = 3.50
 CLASSIC_ADD_RATE = 0.10
 
 ELECTRIC_BASE_RATE = 5.25
 ELECTRIC_ADD_RATE =  0.20
 ELECTRIC_MAX_CHARGE = 100
+
+### SIMULATION CONSTANTS ###
+
+# K = 1        # Calculate probability of a single ride occurring every minute
+LAMBDA = 0.1 # 1 ride / 10 mins or 0.1 ride / min
+LENGTH = 60  # Length of simulation in minutes
