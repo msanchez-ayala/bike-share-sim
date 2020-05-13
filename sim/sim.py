40f71f5f330ec3512d75ea60442fe88293c13f2f
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-05-13 12:14:21 -0400

    allow for multiple bike checkouts at once (generate_checkouts())

--- sim/sim.py
+++ sim/sim.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.distance import cityblock
+from pprint import pprint as pp
 from .station import Station
 from .consts import *
 
@@ -20,6 +21,7 @@ class Simulation:
     - When the simulation is over, compile all logs and print them out to a 
       JSON file.
     """
+
     def __init__(self, length, size = None):
         """
         Sets up the stations and initializes the simulation.
@@ -31,16 +33,167 @@ class Simulation:
         size: [str] (optional) will determine the size of the simulation. To 
               be defined later.
         """
-        self.stations = [
-            Station(0, (0, 0), 1, 1, 0),
-            Station(1, (1, 1), 1)
-        ]
+        self.station_init()
         self.bikes_in_transit = []
         self.bikes_to_dock = []
-        
         self.print_start(length)
         self.run(length)
+        self.print_end()
+
+    def station_init(self):
+        """
+        Sets up the stations for this simulation. Number of stations, size of
+        stations, and number of bikes can be set from consts.py
+        """
+        bikes_per_station = NUM_BIKES // NUM_STATIONS
+
+        station_id = 0
+        bike_id = 0
+        stations = []
+        for _ in range(NUM_STATIONS):
+            stations.append(Station(
+                station_id, 
+                LOCATIONS[station_id], 
+                MEDIUM_STATION,
+                bikes_per_station,
+                bike_id
+            ))
+            station_id += 1
+            bike_id += bikes_per_station
+
+        self.stations = stations
+
+    def run(self, length):
+        """
+        Runs the simulation.
+        """
+        potential_checkouts = self.generate_checkouts(
+            length, self.poisson_thresh
+        )
+
+        # Each loop represents one minute in the simulation    
+        for time, potential_checkout in enumerate(potential_checkouts):
+            print(f'Minute:', time)
+
+            if potential_checkout:
+
+                # Could be more than one checkout this minute, make sure we 
+                # get all of them
+                for _ in range(potential_checkout):
+                    self.check_out_sequence(time)
+            
+            if self.bikes_to_dock:
+                self.check_in_sequence(time)
+            
+            self.update_bikes_in_transit()
+    
+    def check_out_sequence(self, time):
+        """
+        Performs a check-out sequence. 
         
+        Checks for available stations. If exists, finds available dock and 
+        then checks out the bike. If not exists, prints that no bikes were 
+        available at this time.
+
+        If bike was checked out, it is added to bikes in transit list.
+
+        Parameters
+        -----------
+        time: [int] the minute that the current simulation is at.
+        """
+        station_id = self.get_available_station('check out')
+        print('---- Customer tried to check out a bike')
+        
+        # Only proceed if there exists an open dock
+        if station_id != None:
+
+            # print('----', self.stations[station_id].available_bikes,
+            #     'bike(s) available')
+
+            # Find index of an open dock at this station and check out
+            dock_id = self.get_available_dock(
+                self.stations[station_id], 'check out'
+            )
+
+            bike = self.stations[station_id].docks[dock_id].check_out(time)
+            end_station_id = self.determine_destination(station_id)
+            duration = self.determine_trip_duration(
+                station_id, end_station_id
+            )
+
+            # Add that bike to the in_transit list
+            self.bikes_in_transit.append({
+                'bike': bike, 
+                'destination': end_station_id,
+                'time_left': duration
+            })
+
+            print(f'---- Bike checked out of Station: {station_id} Dock: {dock_id}')
+        
+        else:
+            print('---- No available stations for checkout at this time')
+    
+    def check_in_sequence(self, time):
+        """
+        Performs a check-in sequence.
+
+        There are two possibilites: the original destination is open when the
+        bike "arrives", or it isn't Both are covered here.
+        """
+        for bike in self.bikes_to_dock:
+
+            print('---- Customer tried to check in a bike')
+
+            destination_id = bike['destination']
+            dock_id = self.get_available_dock(
+                self.stations[destination_id], 'check in'
+            )
+            # Station is open
+            if dock_id != None:
+                self.stations[destination_id].docks[dock_id].check_in(
+                    bike['bike'], time
+                )
+                print(
+                    '---- Bike checked into Station:', destination_id,
+                    'Dock:', dock_id
+                )
+            
+            # Station isn't open. Pick another station and go there. The origin 
+            # for this new trip is now the old destination (destination_id)
+            else:
+                print('---- Station was full. Customer could not check bike in')
+                print('---- Setting off to find another station with empty slots')
+                end_station_id = self.determine_destination(destination_id)
+                duration = self.determine_trip_duration(
+                    destination_id, end_station_id
+                )
+
+                # Add that bike to the in_transit list
+                self.bikes_in_transit.append({
+                    'bike': bike['bike'],
+                    'destination': end_station_id,
+                    'time_left': duration
+                })
+        self.bikes_to_dock = []
+    
+    def update_bikes_in_transit(self):
+        """
+        Updates self.bikes_in_transit by lowering the 'time_left' key by 1 
+        every minute of the simulation.
+
+        If a bike in transit has 'time_left' = 0, then that bike is moved to 
+        self.bikes_to_dock.
+        """
+        updated_bikes_in_transit = []
+        for bike in self.bikes_in_transit:
+            bike['time_left'] -= 1
+            if bike['time_left'] == 0:
+                self.bikes_to_dock.append(bike)
+            elif bike['time_left'] > 0:
+                updated_bikes_in_transit.append(bike)
+            else:
+                raise ValueError(bike, 'Has negative time left')
+        self.bikes_in_transit = updated_bikes_in_transit
 
     def print_start(self, length):
         """
@@ -62,122 +215,18 @@ class Simulation:
                   'Bikes:', station.available_bikes)
         print('\n')
         print(f'The simulation will cover {length} minutes')
-        print(thin_divider)
+        print(thin_divider)        
     
-    def run(self, length):
+    def print_end(self):
         """
-        Runs the simulation.
+        This prints out the block of text that displays at the end of the
+        output of the simulation.
         """
-        potential_checkouts = self.generate_checkouts(
-            length, self.poisson_thresh)
+        print('-'*50)
+        print('Simulation is over. Total log of bikes is')
+        pp(self.full_log)
+        print('='*50)
 
-        # Each loop represents one minute in the simulation    
-        for time, potential_checkout in enumerate(potential_checkouts):
-            print(f'Minute:', time)
-
-            print('-- Bikes in transit', self.bikes_in_transit)
-            print('-- Bikes to dock', self.bikes_to_dock, '\n')
-
-            ### CHECK OUT SEQUENCE ###
-
-            # If the potential checkout is true, let's try to check a bike out
-            if potential_checkout:
-                station_i = self.get_available_station('check out')
-                print('---- Customer tried to checkout a bike')
-                
-                
-                # Only proceed if there exists an open dock
-                if station_i != None:
-
-
-                    print( 
-                        '----',
-                        self.stations[station_i].available_bikes,
-                        'bike(s) available'
-                    )
-
-
-                    # Find index of an open dock at this station and check out
-                    dock_i = self.get_available_dock(
-                        self.stations[station_i], 'check out'
-                    )
-
-                    # print(
-                    #     '---- Bike from dock:', 
-                    #     self.stations[station_i].docks[dock_i].bike
-                    # )
-
-                    bike = self.stations[station_i].docks[dock_i].check_out(time)
-                    end_station_id = self.determine_destination(station_i)
-                    duration = self.determine_trip_duration(
-                        station_i, end_station_id
-                    )
-
-                    # Add that bike to the in_transit list
-                    self.bikes_in_transit.append({
-                        'bike': bike,
-                        'destination': end_station_id,
-                        'time_left': duration
-                    })
-
-
-                    print(f'---- Bike checked out of Station: {station_i} Dock: {dock_i}')
-                    print(
-                        '---- Bike added to transit list',
-                        self.bikes_in_transit
-                    )
-                    print('---- Full log is now\n----', self.full_log)
-                
-                else:
-                    print('---- No available stations for checkout at this time')
-               
-            
-            ### CHECK IN SEQUENCE ###
-            if self.bikes_to_dock:
-
-                for bike in self.bikes_to_dock:
-                    
-                    # Two possiblities: original destination is open or isn't
-                    destination_id = bike['destination']
-                    dock_id = self.get_available_dock(
-                        self.stations[destination_id], 'check in'
-                    )
-                    # Is open
-                    if dock_id != None:
-                        self.stations[destination_id].docks[dock_id].check_in(
-                            bike['bike'], time
-                        )
-                    
-                    # Isn't open, pick a nearby station and go there. Origin is
-                    # now the old destination (destination_id)
-                    else:
-                        end_station_id = self.determine_destination(station_i)
-                        duration = self.determine_trip_duration(
-                            destination_id, end_station_id
-                        )
-
-                        # Add that bike to the in_transit list
-                        self.bikes_in_transit.append({
-                            'bike': bike,
-                            'destination': end_station_id,
-                            'time_left': duration
-                        })
-                self.bikes_to_dock = []
-
-
-            ### UPDATE BIKES IN TRANSIT ###
-            updated_bikes_in_transit = []
-            for bike in self.bikes_in_transit:
-                bike['time_left'] -= 1
-                if bike['time_left'] == 0:
-                    self.bikes_to_dock.append(bike)
-                elif bike['time_left'] > 0:
-                    updated_bikes_in_transit.append(bike)
-                else:
-                    raise ValueError(bike, 'Has negative time left')
-            self.bikes_in_transit = updated_bikes_in_transit
-
-    
     @property
     def full_log(self):
         """
@@ -226,7 +275,6 @@ class Simulation:
         distance = cityblock(loc_1, loc_2)
         time = distance / SPEED
         return int(time)
-    ### HELPER FUNCTIONS ###
 
     def poisson_thresh(self, k):
         """
@@ -285,8 +333,14 @@ class Simulation:
         # Values to check each probability against
         test_values = np.random.randint(1, 101, length)
 
-        # The test values are lower than the threshold, a bike is checked out
+        # The test values are lower than the threshold, then True and a bike is 
+        # checked out
         checkouts = checkout_thresholds >= test_values
+
+        # Need to arr of booleans into the number of bikes that
+        # will be attempted to check out this minute.
+        stacked = np.stack([ks, checkouts], axis=-1)
+        checkouts = [pair[0] if pair[1] else 0 for pair in stacked]
         
         return checkouts
     
@@ -314,7 +368,6 @@ class Simulation:
             
         # In the event that no stations are available
         return None
-
     
     def get_available_dock(self, station, availability):
         """
@@ -343,23 +396,4 @@ class Simulation:
         # In the event that no docks are available
         return None
 
-
-
-
-
-
-# For every minute in the simulation:
-# First, pick a k from a modified normal distribution. 
-# Second, uses that k to calculate prob of k bikes being checked out
-# Third, picks a number randomly between 0 and 100. If the number is within 
-#     the percentage range, then those number of bikes are checked out that
-#     minute.
-# # For every bike checked out:
-# # First, pick a random station
-# # Second, see if that station has open bikes
-# # Third, pick a destination from any of the other bikes and calculate time 
-# #     based on 10 mph average speed. Put this bike "on hold" until time to 
-# #     dock.
-# # Fourth, if that station is full at docking time, find closest station with 
-# # open slots and repeat third step until we can dock. Calculate price and 
-# # store in log.
+# Calculate price and store in log.
