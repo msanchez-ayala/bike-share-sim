3e7c068ef2e702f616accb258483e80566525ffe
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-05-21 15:08:05 -0400

    convert to pytest

--- test/test_dock.py
+++ test/test_dock.py
@@ -1,68 +1,71 @@
-import unittest
+import pytest
+from mock import Mock
 import numpy as np
 from sim.dock import Dock
 from sim.bike import ClassicBike
-from sim.station import Station
 from sim.consts import CLASSIC_BASE_RATE
 
-class TestDock(unittest.TestCase):
+@pytest.fixture
+def dock():
+    return Dock(0)
 
-    def setUp(self):
-        self.dock = Dock(1)
-        self.loc = tuple(np.array([0, 0]))
-        
-        # Instantiate a bike and station. Ride bike into this dock
-        self.bike = ClassicBike(1)
-        self.station = Station(1, self.loc, 1)
-        self.bike.ride()
+@pytest.fixture
+def classic_bike():
+    mocked_bike = Mock(spec = ClassicBike)
+    mocked_bike.id = 0
+    mocked_bike.trip_id = 0
+    mocked_bike.price.return_value = CLASSIC_BASE_RATE
+    return mocked_bike
+
+class TestDock:
 
     def test_init_value_errors(self):
         """
         Check that all appropriate value errors are thrown by __init__()
         """
-        with self.assertRaises(ValueError):
-            self.dock = Dock(-1)
+        with pytest.raises(ValueError):
+            Dock(-1)
     
     def test_init_type_errors(self):
         """
         Check that all appropriate type errors are thrown by __init__()
         """
-        with self.assertRaises(TypeError):
-            self.dock = Dock('A')             # id
-            self.dock = Dock(1, 'Not a Bike') # ClassicBike instance
+        with pytest.raises(TypeError):
+            Dock('A')             # id
+            Dock(True)            # id
+            Dock(0, 'Not a Bike') # bike arg
     
-    def test_check_in(self):
-        self.assertIsNone(self.dock.bike)
+    def test_check_in(self, dock, classic_bike):
+     
+        assert dock.bike is None, 'Dock was wrongly instantiated with a bike.'
         
         # Check bike in 5 mins after init at station
-        self.dock.check_in(self.bike, 5, 5)
+        dock.check_in(classic_bike, 5, 5)
 
-        self.assertIsInstance(self.bike, ClassicBike)
-        self.assertEqual(self.bike.id, 1)
-        self.assertEqual(self.dock.log[0], {
-            'bike_id': 1,
-            'trip_id': 1, # Fix this if we fix dock code
+        assert dock.log[0] == {
+            'bike_id': 0,
+            'trip_id': 0,
             'end_time': 5,
             'price': CLASSIC_BASE_RATE,
             'duration': 5
-        })
+        }
 
-    def test_check_out(self):
+    def test_check_out(self, dock, classic_bike):
 
         # Check bike in 5 mins after init at station (since the dock was 
         # initialized without a bike in it
 
-        self.dock.check_in(self.bike, 5, 5)
-        
-        self.dock.check_out(10)
+        dock.check_in(classic_bike, 5, 5)
 
-        self.assertIsNone(self.dock.bike)
-        self.assertEqual(self.dock.log[1], {
-            'bike_id': 1,
-            'trip_id': 2, # 2 because setUp() and checkout() both invoke ride()
+        # Add 1 to bike's trip id as check_out() normally would. Must go before
+        # check_out() call because the new trip_id is part of the return stmt.
+        classic_bike.trip_id += 1
+
+        dock.check_out(10)
+
+        assert dock.bike == None
+        assert dock.log[1] == {
+            'bike_id': 0,
+            'trip_id': 1,
             'start_time': 10
-        })
-
-
-if __name__ == '__main__':
-    unittest.main()
+        }
