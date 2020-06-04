8165251b015b72e9c1bd2351bc4360825a39229f
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-06-03 21:19:38 -0400

    introduce setup for CI

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'bike-share-sim',
+    version = '0.1.0',
+    description = 'A simple Python simulation of a bike share such as Citi Bike',
+    author = 'Marco Sanchez-Ayala',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    author_email = 'sanchezayala.marco@gmail.com',
+    install_requires = [
+        'numpy==1.18.2',
+        'pytest==5.3.2',
+        'pytest-mock==3.1.0',
+        'scipy==1.3.1'
+    ]
+)
